5cb60bd8b59db63966c16b384d320425c786aa4e
Rio Kivell <rjckivel@gmail.com>
2017-04-24 12:52:03 -0400

    Added logic for sign-in page, still working through it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+    before_save { email.downcase! }
     validates :name, presence: true, length: {maximum: 64}
     validates :email, presence: true, length: {maximum: 255},
                         format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
                         uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 8 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    root 'application#index`'
-    get 'static_pages/home'
-    get 'static_pages/help'
+    root 'static_pages#home'
+    get '/help',   to: 'static_pages#help'
     get '/signup', to: 'users#new'
+    resources :users
 end
