f45a98919ec1b5f9e46a231a716ef339079140ae
Rio Kivell <rjckivel@gmail.com>
2017-04-24 00:53:25 -0400

    Added validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true, length: {maximum: 64}
+    validates :email, presence: true, length: {maximum: 255},
+                        format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
+                        uniqueness: { case_sensitive: false }
 end
